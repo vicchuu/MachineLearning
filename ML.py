52c9d74a0be4ba2622faa6eeba83e32ee41ec187
vicchuu <vishnumaniplt@gmail.com>
2021-12-24 15:50:47 +0530

    started Learning

--- ML.py
+++ ML.py
@@ -1,1 +1,16 @@
-local guy is vishnu UnboundLocalError
+import numpy as np
+import matplotlib
+import pandas as ps
+
+
+readCSV=ps.read_csv("Data.csv")
+X=readCSV.iloc[:,:].values
+#print(X)
+
+from sklearn.impute import SimpleImputer
+imputer=SimpleImputer(missing_values=np.nan,strategy="mean")
+imputer.fit(X[:,1:3])
+X[:,1:3]=imputer.transform(X[:,1:3])
+print(X)
+
+
