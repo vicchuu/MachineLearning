7f929a12cf35b923bdf4de58060675ae0ee5a86d
vicchuu <vishnumaniplt@gmail.com>
2021-12-28 12:37:57 +0530

    started Learning data preprocessing

--- ML.py
+++ ML.py
@@ -11,6 +11,25 @@ from sklearn.impute import SimpleImputer
 imputer=SimpleImputer(missing_values=np.nan,strategy="mean")
 imputer.fit(X[:,1:3])
 X[:,1:3]=imputer.transform(X[:,1:3])
-print(X)
+#print(X)
+
+from sklearn.preprocessing import LabelEncoder
+
+lencoder=LabelEncoder()
+y=lencoder.fit_transform(X[:,3:4])
+#print(y)
+
+from sklearn.model_selection import train_test_split
+
+Xtrain , Xtest,Ytrain,Ytest=train_test_split(X,y,test_size=0.1,random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+sc=StandardScaler()
+Xtrain[:,1:3]=sc.fit_transform(Xtrain[:,1:3])
+
+
+print (Xtrain)
+print("     *****")
+#print(Ytest)
 
 
