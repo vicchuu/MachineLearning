fce1e1a7915bc1fd022e324a48db72ce22b6d90e
vicchuu <vishnumaniplt@gmail.com>
2021-12-31 16:16:59 +0530

    Multiple Linerar regression

--- multipleRegression.py
+++ multipleRegression.py
@@ -11,4 +11,24 @@ X=dataset.iloc[:,:-1].values
 Y=dataset.iloc[:,-1].values
 
 
-print(X)
+
+
+"""changing column  in city column into binary digit 
+ with the help of sklearn.compose import columnTransfer 
+ sklearn.preprocessing import onehotencoder
+ """
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct=ColumnTransformer(transformers=[("encoder",OneHotEncoder(),[3])],remainder="passthrough")
+X=np.array(ct.fit_transform(X))
+
+
+"""Splitting dara set into training set and test test"""
+from sklearn.model_selection import train_test_split
+xtrain,xtest,ytrain,ytest=train_test_split(X,Y,train_size=0.2,random_state=0)
+
+
+print(xtrain)
+#print("*******")
+#print(help(OneHotEncoder))#
+#print(help(ColumnTransformer))
